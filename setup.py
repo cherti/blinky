50b11c3f87e9b930c91ea0b1ca439bec235b4e63
Jonas Große Sundrup <cherti@letopolis.de>
2018-08-07 13:44:02 +0200

    raise version number for next release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='blinky',
-    version='0.1',
+    version='0.7',
     author='Jonas Große Sundrup',
     author_email='cherti@letopolis.de',
     packages=['blinky'],
