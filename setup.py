a640df554e05b47827cc64c356ced25c2f5b374f
Jonas Große Sundrup <jonas-git@grosse-sundrup.com>
2021-12-13 01:11:59 +0100

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='blinky',
-    version='0.22.1',
+    version='0.23',
     author='Jonas Große Sundrup',
     author_email='cherti@letopolis.de',
     packages=['blinky'],
